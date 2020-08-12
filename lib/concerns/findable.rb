b2fe9158b405de42a091759f3ed2fc5580b9cb67
Milos Popovic <milos_popovic@outlook.com>
2020-08-12 17:51:38 +0000

    Automatically backed up by Learn

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -1,7 +1,31 @@
-module Findable
-  module ClassMethods
-    def find_by_name(name)
-      all.detect{|a| a.name == name}
-    end
-  end
-end
+# module Findable
+#   module ClassMethods
+#     def find_by_name(name)
+#       all.detect{|a| a.name == name}
+#     end
+#   end
+# end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#
--- lib/song.rb
+++ lib/song.rb
@@ -1,32 +1,56 @@
-require 'pry'
+# require 'pry'
+#
+# class Song
+#   extend Memorable::ClassMethods
+#   extend Findable::ClassMethods
+#   attr_accessor :name
+#   attr_reader :artist
+#
+#   @@songs = []
+#
+#   def initialize
+#     @@songs << self
+#   end
+#
+#   # def self.find_by_name(name)
+#   #   @@songs.detect{|a| a.name == name}
+#   # end
+#
+#   def self.all
+#     @@songs
+#   end
+#
+#   def artist=(artist)
+#     @artist = artist
+#   end
+#
+#   def to_param
+#     name.downcase.gsub(' ', '-')
+#   end
+#
+#   extend Memorable
+# end
 
-class Song
-  extend Memorable::ClassMethods
-  extend Findable::ClassMethods
-  attr_accessor :name
-  attr_reader :artist
 
-  @@songs = []
 
-  def initialize
-    @@songs << self
-  end
 
-  # def self.find_by_name(name)
-  #   @@songs.detect{|a| a.name == name}
-  # end
 
-  def self.all
-    @@songs
-  end
 
-  def artist=(artist)
-    @artist = artist
-  end
 
-  def to_param
-    name.downcase.gsub(' ', '-')
-  end
 
-  extend Memorable
-end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#
